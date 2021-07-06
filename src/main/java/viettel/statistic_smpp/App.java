6da7b0e07790d1d29012311339e29ba1478ea78a
VIETTELGROUP\anhld55 <leuaction@gmail.com>
2021-07-06 19:18:15 +0700

    init

---
+++ src/main/java/viettel/statistic_smpp/App.java
@@ -0,0 +1,15 @@
+package viettel.statistic_smpp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+
+    }
+}
---
+++ src/main/java/viettel/statistic_smpp/work/Worker.java
@@ -0,0 +1,8 @@
+package viettel.statistic_smpp.work;
+
+public class Worker {
+
+    private static final int HEARTBEAT_LIVENESS = 3; // 3-5 is reasonable
+
+
+}
