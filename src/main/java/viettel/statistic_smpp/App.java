36efe9b6e02040ef9a08722d79bae323bd8f7409
leuducanh <leuaction@gmail.com>
2021-07-12 05:34:07 +0700

    a

--- src/main/java/viettel/statistic_smpp/App.java
+++ src/main/java/viettel/statistic_smpp/App.java
@@ -1,15 +1,16 @@
 package viettel.statistic_smpp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
 
-
+        SpringApplication.run(App.class, args);
 
     }
 }
